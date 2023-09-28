11febeeaaab22f1a7b2a62deab5a270de36a49dc
Ayush Mehra <ayushmehra998@gmail.com>
2023-09-29 04:05:27 +0530

    Buyfreely website is structured fully with firebase initialization

---
+++ my-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,6 @@
+import style from "./Navbar.module.css"
+export function Navbar(){
+    return (<>
+    <h1 className={style.h1}>Navbar</h1>
+    </>)
+}
---
+++ my-app/src/firebaseinit/firebaseinit.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAkllqr9ojTPKUfHCHpFtIQnIHCcAhYSs0",
+  authDomain: "buyfreely-a92dd.firebaseapp.com",
+  projectId: "buyfreely-a92dd",
+  storageBucket: "buyfreely-a92dd.appspot.com",
+  messagingSenderId: "474194485605",
+  appId: "1:474194485605:web:276ad387a63bab4b74418c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
